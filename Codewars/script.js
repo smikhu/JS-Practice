15ec291f340356c72a382f5a6679c278996ac3e1
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-06 21:21:00 -0400

    finding even or odd numbers

---
+++ Codewars/script.js
@@ -0,0 +1,12 @@
+// Finding even or odd numbers using modulus 
+
+function even_or_odd(number) {
+    if(number % 2 == 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(even_or_odd(100))
+
