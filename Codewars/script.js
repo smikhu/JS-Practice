36d1df2b50f4ecd26fc0585fcc4a04f6c21b225c
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-06 21:52:51 -0400

    find how many vowels input

--- Codewars/script.js
+++ Codewars/script.js
@@ -12,7 +12,26 @@ console.log(even_or_odd(1000))
 
 
 function even_or_odd(number) {
-    return number % 2 ? "Odd" : "Even"
+    return number % 2 === 0 ? "Even" : "Odd"
 }
 
-console.log(even_or_odd(999))
+console.log(even_or_odd("999"))
+
+// Return the number (count) of vowels in the given string. We will consider a,e,i,o,u as vowels for this problem (but not y). The input string will only consists of lower case letters and/or spaces.
+
+function getCount(str) {
+    let vowelsCount = 0;
+    let vowels = ["a", "e", "i", "o", "u"]
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < vowels.length; j++) {
+            if(str[i] === vowels[j]) {
+                vowelsCount++
+            }
+        }
+    }
+    return vowelsCount;
+}
+
+console.log(getCount("alligator"));
+console.log(getCount("jaguar"));
+console.log(getCount("lynx"));
