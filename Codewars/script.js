b35d1a2d120148287b278158e8040fcc392a68d7
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-06 22:00:52 -0400

    vowels count using switch statement

--- Codewars/script.js
+++ Codewars/script.js
@@ -11,11 +11,11 @@ function even_or_odd(number) {
 console.log(even_or_odd(1000))
 
 
-function even_or_odd(number) {
+function even_or_odd2(number) {
     return number % 2 === 0 ? "Even" : "Odd"
 }
 
-console.log(even_or_odd("999"))
+console.log(even_or_odd2("999"))
 
 // Return the number (count) of vowels in the given string. We will consider a,e,i,o,u as vowels for this problem (but not y). The input string will only consists of lower case letters and/or spaces.
 
@@ -35,3 +35,36 @@ function getCount(str) {
 console.log(getCount("alligator"));
 console.log(getCount("jaguar"));
 console.log(getCount("lynx"));
+
+function getCount2(str) {
+    let vowelsCount = 0;
+    let arr = str.split("")
+    for(let i = 0; i < arr.length; i++) {
+        switch(arr[i]) {
+            case "a":
+            case "A":
+                vowelsCount++;
+                break;
+            case "e":
+            case "E":
+                vowelsCount++;
+                break;
+            case "i":
+            case "I":
+                vowelsCount++;
+                break;
+            case "o":
+            case "O":
+                vowelsCount++;
+                break;
+            case "u":
+            case "U":
+                vowelsCount++;
+        }
+    }
+    return vowelsCount;
+}
+
+console.log(getCount2("japan"))
+console.log(getCount2("new zealand"))
+console.log(getCount2("Iraq"))
