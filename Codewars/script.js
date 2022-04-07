5a62ea24b55bf2d1a6c73ba9c528ac650a43f9a2
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-06 22:10:07 -0400

    slightly edited switch statement and working on greeting problem

--- Codewars/script.js
+++ Codewars/script.js
@@ -8,14 +8,14 @@ function even_or_odd(number) {
     }
 }
 
-console.log(even_or_odd(1000))
+// console.log(even_or_odd(1000))
 
 
 function even_or_odd2(number) {
     return number % 2 === 0 ? "Even" : "Odd"
 }
 
-console.log(even_or_odd2("999"))
+// console.log(even_or_odd2("999"))
 
 // Return the number (count) of vowels in the given string. We will consider a,e,i,o,u as vowels for this problem (but not y). The input string will only consists of lower case letters and/or spaces.
 
@@ -32,39 +32,45 @@ function getCount(str) {
     return vowelsCount;
 }
 
-console.log(getCount("alligator"));
-console.log(getCount("jaguar"));
-console.log(getCount("lynx"));
+// console.log(getCount("alligator"));
+// console.log(getCount("jaguar"));
+// console.log(getCount("lynx"));
 
 function getCount2(str) {
     let vowelsCount = 0;
     let arr = str.split("")
+    console.log(arr)
     for(let i = 0; i < arr.length; i++) {
         switch(arr[i]) {
             case "a":
             case "A":
-                vowelsCount++;
-                break;
             case "e":
             case "E":
-                vowelsCount++;
-                break;
             case "i":
             case "I":
-                vowelsCount++;
-                break;
             case "o":
             case "O":
-                vowelsCount++;
-                break;
             case "u":
             case "U":
                 vowelsCount++;
+                break;
         }
     }
     return vowelsCount;
 }
 
-console.log(getCount2("japan"))
-console.log(getCount2("new zealand"))
-console.log(getCount2("Iraq"))
+// console.log(getCount2("japan"))
+// console.log(getCount2("new zealand"))
+// console.log(getCount2("Iraq"))
+
+
+// Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake. Can you help her?
+
+function greet(name) {
+    return "Hello, " + name + "!";
+    if (name === "Johnny") {
+        return "Hello, my love!";
+    }
+}
+
+console.log(greet())
