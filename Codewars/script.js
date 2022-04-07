bbffea26c55ab5445267f503423b57042aa0de9d
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-07 00:49:10 -0400

    operation function multiple ways

--- Codewars/script.js
+++ Codewars/script.js
@@ -330,5 +330,61 @@ function positiveSum(arr) {
     return sum
 }
 
-console.log(positiveSum([1, -4, 7, 12]))
-console.log(positiveSum([1, -4, 7, 12]))
+// console.log(positiveSum([1, -4, 7, 12]))
+// console.log(positiveSum([1, -4, 7, 12]))
+
+
+
+
+// Your task is to create a function that does four basic mathemetical operations. The function should take three arguments - operation(string/char), value1(number), value2(number). The function should return result of numbers after applying the chosen operation.
+
+function basicOp(operation, value1, value2) {
+    if (operation === "+") {
+        return value1 + value2
+    } else if (operation === "-") {
+        return value1 - value2
+    } else if (operation === "*") {
+        return value1 * value2
+    } else if (operation === "/") {
+        return value1 / value2
+    } else {
+        return "wrong arguments given"
+    }
+}
+
+// console.log(basicOp("+", 4, 7))
+// console.log(basicOp("-", 15, 18))
+// console.log(basicOp("*", 5, 5))
+// console.log(basicOp("/", "49", "7"))
+// console.log(basicOp("hello", 49, 7))
+
+
+function basicOp2(op, v1, v2) {
+    switch(op) {
+        case "+":
+            return v1 + v2
+        case "-":
+            return v1 - v2
+        case "*":
+            return v1 * v2
+        case "/":
+            return v1 / v2
+        default:
+            return 0;
+    }
+}
+
+// console.log(basicOp2("+", 4, 7))
+// console.log(basicOp2("-", 15, 18))
+// console.log(basicOp2("*", 5, 5))
+// console.log(basicOp2("/", "49", "7"))
+
+
+function basicOp3(op, v1, v2) {
+    return eval(v1+op+v2)
+}
+
+// console.log(basicOp3("+", 4, 7))
+// console.log(basicOp3("-", 15, 18))
+// console.log(basicOp3("*", 5, 5))
+// console.log(basicOp3("/", 49, 7))
