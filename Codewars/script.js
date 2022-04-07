0b983c264e29d09c43ad9e3662a14bca62bf9d7c
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-06 21:22:47 -0400

    changed double equal to triple equal to check both value and data type

--- Codewars/script.js
+++ Codewars/script.js
@@ -1,12 +1,12 @@
 // Finding even or odd numbers using modulus 
 
 function even_or_odd(number) {
-    if(number % 2 == 0) {
-        return true
+    if(number % 2 === 0) {
+        return "Even"
     } else {
-        return false
+        return "Odd"
     }
 }
 
-console.log(even_or_odd(100))
+console.log(even_or_odd(1000))
 
