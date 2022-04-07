59fc8d861af39dafd01d5dd5b2585e23282ab31e
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-06 22:15:27 -0400

    greet function with if else and ternary operator

--- Codewars/script.js
+++ Codewars/script.js
@@ -67,10 +67,19 @@ function getCount2(str) {
 // Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake. Can you help her?
 
 function greet(name) {
-    return "Hello, " + name + "!";
     if (name === "Johnny") {
         return "Hello, my love!";
+    } else {
+        return "Hello, " + name + "!";
     }
 }
 
-console.log(greet())
+console.log(greet("Steven"))
+console.log(greet("Johnny"))
+
+function greet2(name) {
+    return name === "Johnny" ? "Hello, my love!" : `Hello, ${name}!`;
+}
+
+console.log(greet2("Steven"))
+console.log(greet2("Johnny"))
