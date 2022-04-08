2d45dccc968c9e0f08e001cd7bb95589684ed09e
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-08 13:45:51 -0400

    human years to dog/cat years conversion

--- Codewars/script.js
+++ Codewars/script.js
@@ -563,3 +563,47 @@ if (year % 100 === 0) {
 console.log(century2(1900))
 console.log(century2(1901))
 console.log(century2(89))
+
+
+
+
+// I have a cat and a dog.
+// I got them at the same time as kitten/puppy. That was humanYears ago.
+// Return their respective ages now as [humanYears, catYears, dogYears]
+// humanYears >= 1 / humanYears are whole numbers only
+// Cat Years - 15 cat years for first year/ +9 cat years for second year/ +4 cat years for each year after that
+// Dog Years - 15 dog years for the first year/ +9 dog years for second year/ +5 dog years for each year after that
+
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+    let catsAge;
+    let dogsAge;
+
+    if (humanYears === 1) {
+        return [humanYears, 15, 15];
+    }
+
+    if (humanYears === 2) {
+        return [humanYears, 24, 24]
+    }
+
+    const excessYears = humanYears - 2;
+    const extraCatYears = excessYears * 4;
+    const extraDogYears = excessYears * 5;
+
+    return [humanYears, 24 + extraCatYears, 24 + extraDogYears]
+
+}
+
+console.log(humanYearsCatYearsDogYears(3))
+
+
+
+var humanYearsCatYearsDogYears2 = function(humanYears) {
+    if (humanYears == 1) return [1, 15, 15]
+    if (humanYears == 2) return [2, 24, 24]
+    return [humanYears, (humanYears - 2) * 4 + 24, (humanYears - 2) * 5 + 24]
+}
+
+console.log(humanYearsCatYearsDogYears2(3))
+console.log(humanYearsCatYearsDogYears2(5))
