8f227eff880352b59d671cf41104343ced3f27b4
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-09 01:15:22 -0400

    palindrome

--- JavaScript-Cardio-Session/script.js
+++ JavaScript-Cardio-Session/script.js
@@ -3,6 +3,7 @@
 // ex. reverseString('hello') === 'olleh'
 
 function reverseString(str) {
+    console.log(str)
     const splitting = str.split('')
     console.log(splitting)
     const reverse = splitting.reverse()
@@ -11,4 +12,64 @@ function reverseString(str) {
     return joining
 }
 
-console.log(reverseString('hello'))
+// console.log(reverseString('hello'))
+
+
+function reverseString2(str) {
+    let revString = ''
+    for (let i = str.length - 1; i >= 0; i--) {
+        revString = revString + str[i];
+    }
+    return revString
+}
+
+// console.log(reverseString2('hello'))
+
+
+function reverseString3(str) {
+    let revString = ''
+    for (let i of str) {
+        // console.log(i)
+        revString = i + revString;
+        // console.log(revString)
+    }
+    return revString;
+}
+
+// console.log(reverseString3('hello'))
+
+
+function reverseString4(str) {
+    let revString = '';
+    str.split('').forEach(function(char) {
+        console.log(char)
+        revString = char + revString;
+        
+    })
+    return revString
+}
+
+console.log(reverseString4('hello'))
+
+// CHALLENGE 2: VALIDATE A PALINDROME
+// Return true if palindrome and flase if not
+// ex. isPalindrome('racecar') === 'true', isPalindrome('hello') === false
+
+function isPalindrome(str) {
+    console.log(str)
+    let splitting = str.split('')
+    console.log(splitting)
+    let reverse = splitting.reverse()
+    console.log(reverse)
+    let joining = reverse.join('')
+    console.log(joining)
+
+    if (joining === str) { 
+        return "is a palindrome!"
+    } else {
+        return "not a palindrome!"
+    }
+
+}
+
+console.log(isPalindrome('racecar'))
