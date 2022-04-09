b3e6fb55a1a0b706746ee76f7ace74c61a9ac6bd
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-09 00:40:19 -0400

    reversing a string

--- JavaScript-Cardio-Session/script.js
+++ JavaScript-Cardio-Session/script.js
@@ -1,1 +1,14 @@
-console.log("Hello")
+// CHALLENGE 1: REVERSE A STRING
+// Return a string in reverse
+// ex. reverseString('hello') === 'olleh'
+
+function reverseString(str) {
+    const splitting = str.split('')
+    console.log(splitting)
+    const reverse = splitting.reverse()
+    console.log(reverse)
+    const joining = reverse.join('')
+    return joining
+}
+
+console.log(reverseString('hello'))
