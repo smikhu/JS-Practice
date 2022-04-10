d2a4626fefd203be054d57c8b76ac51ca2427e83
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-09 21:19:38 -0400

    anagram

--- javascript-cardio-session-2/script.js
+++ javascript-cardio-session-2/script.js
@@ -111,3 +111,30 @@ function flattenArray3(arrays) {
 }
 
 console.log(flattenArray3([[1, 2], [3, 4], [5, 6], [7]]))
+
+
+
+
+
+// CHALLENGE 4: ANAGRAM
+// Return true if anagram and false if not
+// ex. 'elbow' === 'below'
+// ex. 'Dormitory' === 'dirty room##'
+
+function isAnagram(str1, str2) {
+    return formatStr(str1) === formatStr(str2)
+}
+
+
+// Helper function
+function formatStr(str) {
+    return str
+        .replace(/[^\w]/g, '')
+        .toLowerCase()
+        .split('')
+        .sort()
+        .join('')
+}
+
+console.log(isAnagram('elbow', 'below'))
+console.log(isAnagram('Dormitory', 'dirty room##'))
