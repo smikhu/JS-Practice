eeccfdd172015c154c30f7431464b5d9aa788c4c
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-10 19:59:30 -0400

    add all prime numbers

--- javascript-cardio-session-3/script.js
+++ javascript-cardio-session-3/script.js
@@ -6,7 +6,7 @@ function addAll() {
     // ES5 arguments & for loop
     let args = Array.prototype.slice.call(arguments)
     let total = 0;
-    for (i = 0; i < args.length; i++) {
+    for (let i = 0; i < args.length; i++) {
         total += args[i]
     }
     return total
@@ -27,3 +27,39 @@ function addAll2(...numbers) {
 
 console.log(addAll2(2, 5, 6, 7))
 console.log(addAll2(2, 5, 6, 7, 8))
+
+
+function addAll3(...numbers) {
+    return numbers.reduce((acc, cur) => acc + cur)
+}
+
+console.log(addAll3(2, 5, 6, 7))
+console.log(addAll3(2, 5, 6, 7, 7))
+
+
+// CHALLENGE 2: SUM ALL PRIMES
+// Pass in a number to loop up to and all of the prime numbers. A prime number is a whole number greater than 1 whose only factors are 1 and itself
+// ex. sumAllPrimes(10) == 17
+
+function sumAllPrimes(num) {
+    let total = 0;
+
+    function checkForPrime(i) {
+        for(let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                return false
+            }
+        }
+        return true
+    }
+
+    for (let i = 2; i <= num; i++) {
+        if(checkForPrime(i)) {
+            total += i
+        }
+    }
+    return total;
+}
+
+console.log(sumAllPrimes(10))
+console.log(sumAllPrimes(25))
