161eac8c7845268559ab25488257020b6a8079cd
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-09 21:02:47 -0400

    array chunks

--- javascript-cardio-session-2/script.js
+++ javascript-cardio-session-2/script.js
@@ -26,5 +26,55 @@ function longestWord(sen) {
     }
 }
 
-console.log(longestWord('Hello, my name is Brad'))
-console.log(longestWord('Hello there, my name is Brad'))
+// console.log(longestWord('Hello, my name is Brad'))
+// console.log(longestWord('Hello there, my name is Brad'))
+
+
+// CHALLENGE 2: ARRAY CHUNKING
+// Split an array into chunked arrays of a specific length
+// ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 3) === [[1, 2, 3],[4, 5, 6], [7]]
+// ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6], [7]]
+
+function chunkArray(arr, len) {
+    // initialize chunked arr
+    const chunkedArr = [];
+    // set index
+    let i = 0;
+
+    // loop while index is less than the array length
+    while (i < arr.length) {
+        // slice out from the index to the index + the chunk length and push on to the chunkedArr
+        chunkedArr.push(arr.slice(i, i + len))
+        // increment by chunk length
+        i += len;
+    }
+    return chunkedArr
+}
+
+// console.log(chunkArray([1, 2, 3, 4, 5, 6, 7], 3))
+// console.log(chunkArray([1, 2, 3, 4, 5, 6, 7], 2))
+
+
+
+
+function chunkArray2(arr, len) {
+    // init chunked arr
+    const chunkedArr = [];
+
+    // loop through arr
+    arr.forEach(function(val) {
+        // get last element
+        const last = chunkedArr[chunkedArr.length - 1]
+        
+        // check if last and if last length is equal to the chunk length
+        if (!last || last.length === len) {
+            chunkedArr.push([val])
+        } else {
+            last.push([val])
+        }
+    })
+    return chunkedArr
+}
+
+console.log(chunkArray2([1, 2, 3, 4, 5, 6, 7], 3))
+console.log(chunkArray2([1, 2, 3, 4, 5, 6, 7], 2))
