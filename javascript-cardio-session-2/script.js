593091ee83710c22e7bb26a16f1b80c42e700cb3
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-09 21:27:15 -0400

    letter changes

--- javascript-cardio-session-2/script.js
+++ javascript-cardio-session-2/script.js
@@ -138,3 +138,31 @@ function formatStr(str) {
 
 console.log(isAnagram('elbow', 'below'))
 console.log(isAnagram('Dormitory', 'dirty room##'))
+
+
+
+
+
+// CHALLENGE 5: LETTER CHANGES
+// Change every letter of the string to the one that follows it and capitalize the vowels
+// Z should turn to A
+// ex. 'hello there' === 'Ifmmp UIfsf'
+
+
+function letterChanges(str) {
+    let newStr = str.toLowerCase().replace(/[a-z]/gi, function(char) {
+        if (char === 'z' || char === 'Z') {
+            return 'a';
+        } else {
+            return String.fromCharCode(char.charCodeAt() + 1)
+        }
+    })
+
+    newStr = newStr.replace(/a|e|i|o|u/gi, function(vowel) {
+        return vowel.toUpperCase()
+    })
+
+    return newStr
+}
+
+console.log(letterChanges('Hello There'))
