9148a2333e42046d79e85ed584ffcb3ca582b65a
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-11 22:49:13 -0400

    sorted by height using ternary operator

--- javascript-cardio-session-3/script.js
+++ javascript-cardio-session-3/script.js
@@ -128,3 +128,21 @@ function sortByHeight(a) {
 }
 
 console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]))
+
+
+
+
+function sortByHeight2(a) {
+    const arr1 = [];
+    const arr2 = [];
+
+    a.forEach((val, i) => val === -1 ? arr1.push(i) : arr2.push(val));
+
+    const sortArr = arr2.sort((a, b) => a - b);
+
+    arr1.forEach((val, i) => sortArr.splice(arr1[i], 0, -1));
+
+    return sortArr;
+}
+
+console.log(sortByHeight2([-1, 150, 190, 170, -1, -1, 160, 180]))
