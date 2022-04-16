898bd06532a4b93de46f5c93fc225a0a2fddce53
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-16 17:52:00 -0400

    return half of given string length of even length

--- Codewars/script.js
+++ Codewars/script.js
@@ -785,3 +785,12 @@ console.log(newString("zomhahahabie"))
 
 
 
+// Write a JS program to extract the first half of a string of even length.
+
+
+function firstHalf(str) {
+    return str.slice(0, str.length / 2)
+}
+
+console.log(firstHalf("zombie"))
+console.log(firstHalf("catapult"))
