0281a917ccd31f286ad9cb4b79a0219e279e436a
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-16 21:12:26 -0400

    elemets in a collection

--- Codewars/script.js
+++ Codewars/script.js
@@ -1045,3 +1045,28 @@ function getRandomHexColor() {
 }
 
 console.log(getRandomHexColor())
+
+
+
+
+
+// Write a JS function that returns true if the provided predicate function returns true for all elements in a collection, false otherwise.
+
+
+console.log([1,2,3,4,5].every(x => x > 0))
+console.log([1,2,3,4,5].every(x => x > 3))
+
+
+
+function isEveryElem(arr, fn) {
+    for (let i = 0; i < arr.length; i += 1) {
+        if (!fn(arr[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+console.log(isEveryElem([1, 2, 3, 4, 5], (x) => x > 0))
+console.log(isEveryElem([1, 2, 3, 4, 5], (x) => x > 3))
