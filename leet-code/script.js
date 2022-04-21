f051450b4ca8ab8a65670a2810eb0cab0ddfb0a7
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-20 20:42:39 -0400

    max profit

--- leet-code/script.js
+++ leet-code/script.js
@@ -43,10 +43,10 @@ var twoSum = function(nums, target) {
     }
 };
 
-console.log(twoSum([2, 7, 11, 15], 9))
-console.log(twoSum([2, 7, 11, 15], 22))
-console.log(twoSum([2, 7, 11, 15], 26))
-console.log(twoSum([2, 7, 11, 15], 17))
+// console.log(twoSum([2, 7, 11, 15], 9))
+// console.log(twoSum([2, 7, 11, 15], 22))
+// console.log(twoSum([2, 7, 11, 15], 26))
+// console.log(twoSum([2, 7, 11, 15], 17))
 
 
 
@@ -64,10 +64,10 @@ const twoSum2 = function(nums, target) {
 }
 
 
-console.log(twoSum2([2, 7, 11, 15], 9))
-console.log(twoSum2([2, 7, 11, 15], 22))
-console.log(twoSum2([2, 7, 11, 15], 26))
-console.log(twoSum2([2, 7, 11, 15], 17))
+// console.log(twoSum2([2, 7, 11, 15], 9))
+// console.log(twoSum2([2, 7, 11, 15], 22))
+// console.log(twoSum2([2, 7, 11, 15], 26))
+// console.log(twoSum2([2, 7, 11, 15], 17))
 
 
 
@@ -83,10 +83,10 @@ const twoSum3 = function(nums, target) {
 }
 
 
-console.log(twoSum3([2, 7, 11, 15], 9))
-console.log(twoSum3([2, 7, 11, 15], 22))
-console.log(twoSum3([2, 7, 11, 15], 26))
-console.log(twoSum3([2, 7, 11, 15], 17))
+// console.log(twoSum3([2, 7, 11, 15], 9))
+// console.log(twoSum3([2, 7, 11, 15], 22))
+// console.log(twoSum3([2, 7, 11, 15], 26))
+// console.log(twoSum3([2, 7, 11, 15], 17))
 
 
 
@@ -107,7 +107,7 @@ function duplicate(nums) {
     return false;
 }
 
-console.log(duplicate([1, 2, 3, 4, 5, 6, 7, 8, 20, 21, 22, 23, 1]))
+// console.log(duplicate([1, 2, 3, 4, 5, 6, 7, 8, 20, 21, 22, 23, 1]))
 
 
 
@@ -122,4 +122,39 @@ function duplicate2(nums) {
 }
 
 
-console.log(duplicate2([1, 2, 3, 4, 5, 6, 7, 8, 20, 21, 22, 23]))
+// console.log(duplicate2([1, 2, 3, 4, 5, 6, 7, 8, 20, 21, 22, 23]))
+
+
+
+
+
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+// Example 2:
+
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+
+// Constraints:
+
+// 1 <= prices.length <= 105
+// 0 <= prices[i] <= 104
+
+
+ function maxProfit(prices) {
+     
+ }
