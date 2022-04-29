0745891f0669d23e6ebe3e0556cbf8bfeca48ba3
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-29 19:33:33 -0400

    exercise 1 completed

--- random-js-exercises/script.js
+++ random-js-exercises/script.js
@@ -6,6 +6,10 @@
   Highlight all of the words over 8 characters long in the paragraph text (with a yellow background for example)
 */
 
+const paragraph = document.querySelector('p');
+paragraph.innerHTML = paragraph.innerText.split(' ').map(word => word.length > 8 ? `<span style="background-color: yellow">${word}</span>"` : word).join(' ');
+
+
 /*
   Exercise 02
   -----------
