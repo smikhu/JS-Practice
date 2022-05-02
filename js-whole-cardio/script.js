52b34fc080424107aeea01763f480fb0fb8a8e1f
Steven Mikhu <StevenMikhu@gmail.com>
2022-05-01 21:00:43 -0400

    move every letter forward once

--- js-whole-cardio/script.js
+++ js-whole-cardio/script.js
@@ -53,4 +53,12 @@ console.log(getFileExtension2('webpack.config.jpeg'))
 
 
 
-// 
+// Write a JS program to replace every character in a given string with the character following it in the alphabet.
+
+const moveCharsForward = (str) => 
+    str
+    .split('')
+    .map(char => String.fromCharCode(char.charCodeAt(0) + 1))
+    .join('');
+
+console.log(moveCharsForward('abcde'))
