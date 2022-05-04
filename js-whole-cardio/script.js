800dca6bbf3b3e2fbdd8c8598a320fe2b02eebb9
Steven Mikhu <StevenMikhu@gmail.com>
2022-05-04 19:40:56 -0400

    reversing a string using a decrementing for loop

--- js-whole-cardio/script.js
+++ js-whole-cardio/script.js
@@ -108,5 +108,18 @@ function reverseString(str) {
     return str.split('').reverse().join('')
 }
 
-
 console.log(reverseString('hello'))
+
+
+
+function reverseString2(str) {
+    let revString = '';
+    for(let i = str.length - 1; i >= 0; i--) {
+        revString = revString + str[i]
+    }
+    return revString
+}
+
+
+console.log(reverseString2('hello'))
+console.log(reverseString2('crocodile'))
