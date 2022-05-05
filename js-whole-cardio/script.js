0557ce873478db1d384bb0798dcf8569f8c83486
Steven Mikhu <StevenMikhu@gmail.com>
2022-05-05 17:53:04 -0400

    longest word

--- js-whole-cardio/script.js
+++ js-whole-cardio/script.js
@@ -368,3 +368,61 @@ function fizzBuzz() {
 }
 
 console.log(fizzBuzz())
+
+
+
+
+
+
+
+// CHALLENGE 1: LONGEST WORD
+// Return the longest word of a string
+// ex. longestWord('Hi there, my name is Brad') === 'there,'
+
+function longestWord(sen) {
+    const wordArr = sen.toLowerCase().match(/[a-z0-9]+/g);
+
+    const sorted = wordArr.sort(function(a, b) {
+        return b.length - a.length;
+    })
+
+    const longestWordArr = sorted.filter(function(word) {
+        return word.length === sorted[0].length
+    })
+
+    if(longestWordArr.length === 1) {
+        return longestWordArr[0]
+    } else {
+        return longestWordArr
+    }
+}
+
+console.log(longestWord('Helloo there, my name is Brad'))
+    
+  
+// CHALLENGE 2: ARRAY CHUNKING
+// Split an array into chunked arrays of a specific length
+// ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 3) === [[1, 2, 3],[4, 5, 6],[7]]
+// ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6],[7]]
+
+
+
+// CHALLENGE 3: FLATTEN ARRAY
+// Take an array of arrays and flatten to a single array
+// ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
+
+
+
+// CHALLENGE 4: ANAGRAM
+// Return true if anagram and false if not
+// ex. 'elbow' === 'below'
+// ex. 'Dormitory' === 'dirty room##'
+
+
+
+// CHALLENGE 5: LETTER CHANGES
+// Change every letter of the string to the one that follows it and capitalize the vowels
+// Z should turn to A
+// ex. 'hello there' === 'Ifmmp UIfsf'
+  
+ 
