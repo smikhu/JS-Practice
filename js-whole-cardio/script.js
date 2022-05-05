567bb1b7015b80fff5b1aff1d2c755f20c18bd9b
Steven Mikhu <StevenMikhu@gmail.com>
2022-05-04 20:21:15 -0400

    finding max character using for in loop

--- js-whole-cardio/script.js
+++ js-whole-cardio/script.js
@@ -313,3 +313,36 @@ function capitalizeLetters3(str) {
 
 console.log(capitalizeLetters3('i love javascript'))
 console.log(capitalizeLetters3('i love javaSCRipt'))
+
+
+
+
+
+
+// MAX CHARACTER
+
+function maxCharacter(str) {
+    const charMap = {};
+    let maxNum = 0;
+    let maxChar = '';
+
+    str.split('').forEach(function (char) {
+        if(charMap[char] ) {
+            charMap[char]++
+        } else {
+            charMap[char] = 1;
+        }
+    })
+
+    for(let char in charMap) {
+        if(charMap[char] > maxNum) {
+            maxNum = charMap[char]
+            maxChar = char;
+        }
+    }
+    return maxChar
+}
+
+
+console.log(maxCharacter('javascript'))
+console.log(maxCharacter('javascriptttttt'))
