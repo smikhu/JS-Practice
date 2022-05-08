f5024f42fff6f70f6c4d0f26950362a43d0a49cf
Steven Mikhu <StevenMikhu@gmail.com>
2022-05-07 20:53:27 -0400

    letter changes using replace

--- js-whole-cardio/script.js
+++ js-whole-cardio/script.js
@@ -514,4 +514,23 @@ console.log(isAnagram('dirty rooms', 'Dormitory'))
 // Z should turn to A
 // ex. 'hello there' === 'Ifmmp UIfsf'
   
+
+function letterChanges(str) {
+    let newStr = str.toLowerCase().replace(/[a-z]/gi, function(char) {
+        if(char === 'z' || char === 'Z') {
+            return 'a';
+        } else {
+            return String.fromCharCode(char.charCodeAt() + 1)
+        }
+    })
+
+    newStr = newStr.replace(/a|e|i|o|u/gi, function(vowel) {
+        return vowel.toUpperCase();
+    })
+
+    return newStr;
+}
+
+console.log(letterChanges('Hello there'))
+console.log(letterChanges('Helloz therez'))
  
