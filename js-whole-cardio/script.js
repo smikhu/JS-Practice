1b041760557574221fb78eda1be8fcd98257f02b
Steven Mikhu <StevenMikhu@gmail.com>
2022-05-15 17:42:49 -0400

    seek & destroy using array from and filter

--- js-whole-cardio/script.js
+++ js-whole-cardio/script.js
@@ -602,3 +602,23 @@ function sumAllPrimes(num) {
 }
 
 console.log(sumAllPrimes(10))
+
+
+
+
+// CHALLENGE 3 : SEEK & DESTROY
+
+
+function seekAndDestroy(arr) {
+    const args = Array.from(arguments);
+
+    function filterArr(arr) {
+        //Return true if NOT in array
+        return args.indexOf(arr) === -1
+    }
+
+    return arr.filter(filterArr);
+}
+
+console.log(seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6))
+console.log(seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6, 'hello'))
