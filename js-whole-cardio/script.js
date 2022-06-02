13c5f0b5eb17d0c49570bb80297a6ca6c238273b
Steven Mikhu <StevenMikhu@gmail.com>
2022-06-01 22:46:34 -0400

    sort by height using forEach and splice

--- js-whole-cardio/script.js
+++ js-whole-cardio/script.js
@@ -633,3 +633,40 @@ function seekAndDestroy2(arr, ...rest) {
 
 console.log(seekAndDestroy2([2, 3, 4, 6, 6, 'hello'], 2, 6))
 console.log(seekAndDestroy2([2, 3, 4, 6, 6, 'hello'], 2, 6, 'hello'))
+
+
+
+
+
+
+
+// CHALLENGE 4: SORT BY HEIGHT
+// Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
+// ex.
+// a = [-1, 150, 190, 170, -1, -1, 160, 180]
+// sortByHeight(a) == [-1, 150, 160, 170, -1, -1, 180, 190]
+
+function sortByHeight(a) {
+    const arr1 = [];
+    const arr2 = [];
+    
+    a.forEach((val, i) => {
+        if(val === -1) {
+            arr1.push(i);
+        } else {
+            arr2.push(val);
+        }
+    });
+    // console.log(arr1, arr2)
+
+    const sortArr = arr2.sort((a, b) => a - b)
+
+    // console.log(sortArr)
+
+    arr1.forEach((val, i) => sortArr.splice(arr1[i], 0, -1))
+
+    return sortArr;
+
+}
+
+// console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]))
