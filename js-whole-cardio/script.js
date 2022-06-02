169460e737e6ba4bbffb54bfc14550b1dc491556
Steven Mikhu <StevenMikhu@gmail.com>
2022-06-01 22:48:08 -0400

    even and odd sums using foreach

--- js-whole-cardio/script.js
+++ js-whole-cardio/script.js
@@ -721,3 +721,25 @@ function missingLetters(str) {
 console.log(missingLetters("abce"))
 console.log(missingLetters("abcdefghjklmno"))
 console.log(missingLetters("abcdefghijklmnopqrstuvwxyz"))
+
+
+
+
+
+
+// CHALLENGE 6: EVEN & ODD SUMS
+// Take in an array and return an array of the sums of even and odd numbers
+// ex.
+// evenOddSums([50, 60, 60, 45, 71] == [170, 116])
+
+function evenOddSums(arr) {
+    let evenSum = 0;
+    let oddSum = 0;
+
+    arr.forEach(num => (num % 2 === 0 ? (evenSum += num) : (oddSum += num)))
+
+    return [evenSum, oddSum]
+}
+
+
+console.log(evenOddSums([50, 60, 60, 45, 71]))
