a206920d97a5e919a77b00e4152e4426b27da531
Steven Mikhu <StevenMikhu@gmail.com>
2022-06-05 14:41:51 -0400

    special greeting

--- js-whole-cardio/script.js
+++ js-whole-cardio/script.js
@@ -953,3 +953,19 @@ function getCount(str) {
   }
   
   // console.log(getCount("abracadabra"))
+
+
+
+
+
+
+  
+function greet(name){
+    if(name === "Johnny") {
+      return "Hello, my love!";
+    } else {
+      return "Hello, " + name + "!";
+    }
+  }
+  
+console.log(greet("Johnny"))
