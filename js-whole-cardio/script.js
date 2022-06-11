072c9997ec34edbc75ea9b1484340c6069de902e
Steven Mikhu <StevenMikhu@gmail.com>
2022-06-10 22:46:45 -0400

    finding positive sum using for loop

--- js-whole-cardio/script.js
+++ js-whole-cardio/script.js
@@ -1093,3 +1093,19 @@ function isDivisible(n, x, y) {
    
   // console.log(removeChar("Steven")) 
   
+
+
+
+
+
+  function positiveSum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > 0) {
+        sum = sum + arr[i]
+      }
+    }
+    return sum
+  }
+  
+  // console.log(positiveSum([1, -4, 7, 12]))
