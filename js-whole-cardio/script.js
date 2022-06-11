6c30c9d8b2dc80080fa8c96adaff4d9d1e0ac938
Steven Mikhu <StevenMikhu@gmail.com>
2022-06-10 22:47:13 -0400

    finding operation using if else statements

--- js-whole-cardio/script.js
+++ js-whole-cardio/script.js
@@ -1109,3 +1109,25 @@ function isDivisible(n, x, y) {
   }
   
   // console.log(positiveSum([1, -4, 7, 12]))
+
+
+
+
+
+
+  function basicOp(operation, value1, value2) {
+    if (operation === "+") {
+      return value1 + value2
+    } else if (operation === "-") {
+      return value1 - value2
+    } else if (operation === "*") {
+      return value1 * value2
+    } else if (operation === "/") {
+      return value1 / value2
+    }
+  }
+  
+  // console.log(basicOp('+', 4, 7))
+  // console.log(basicOp('-', 15, 18))
+  // console.log(basicOp('*', 5, 5))
+  // console.log(basicOp('/', 49, 7))
