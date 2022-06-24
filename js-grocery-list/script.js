e112dbc3c4e4e5f8efc388a570c8ae7f07931f07
Steven Mikhu <StevenMikhu@gmail.com>
2022-06-23 20:26:14 -0400

    grocery list fully functional

--- js-grocery-list/script.js
+++ js-grocery-list/script.js
@@ -1,1 +1,27 @@
-console.log('testing')
+const groceries = document.getElementsByClassName('groceries')[0];
+const pencil = document.getElementById('pencil');
+const allItems = document.getElementById('allItems');
+const userInput = document.getElementById('userInput');
+
+pencil.addEventListener('click', () => {
+    allItems.innerHTML = ''
+})
+
+userInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        addItem();
+    }
+})
+
+const addItem = () => {
+    let h2 = document.createElement('h2');
+    h2.innerHTML = '-' + userInput.value;
+
+    h2.addEventListener('click', () => {
+        h2.style.textDecoration = 'line-through';
+    })
+
+    allItems.insertAdjacentElement('beforeend', h2);
+
+    userInput.value = '';
+}
