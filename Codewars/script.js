b1f342638d5e6ee7c31322187a4d23cb72842002
Steven Mikhu <StevenMikhu@gmail.com>
2022-07-22 22:30:55 -0400

    completed exercise 1

--- Codewars/script.js
+++ Codewars/script.js
@@ -1219,7 +1219,8 @@ function noOdds(values) {
     return values.filter(val => val % 2 === 0);
 }
 
-console.log(noOdds([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+// console.log(noOdds([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+// console.log(noOdds([1, 3, 5, 7, 9, 10]))
 
 
 
@@ -1233,6 +1234,46 @@ function noOdds2(values) {
     return arr
 }
 
-console.log(noOdds2([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
-console.log(noOdds2([0, 1, 2, 3, 5, 6, 7, 8, 9, 10]))
-console.log(noOdds2([0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 231, 531, 221, 444]))
+// console.log(noOdds2([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+// console.log(noOdds2([0, 1, 2, 3, 5, 6, 7, 8, 9, 10]))
+// console.log(noOdds2([0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 231, 531, 221, 444]))
+
+
+const orders = [
+    { orderId: '123', customerId: '123', deliveryDate: '01-01-2020', delivered: true, items: [
+        { productId: '123', price: 55 },
+        { productId: '234', price: 30 },
+    ]},
+    { orderId: '234', customerId: '234', deliveryDate: '01-02-2020', delivered: false, items: [
+        { productId: '234', price: 30 },
+    ]},
+    { orderId: '345', customerId: '234', deliveryDate: '05-01-2020', delivered: true, items: [
+        { productId: '567', price: 30 },
+        { productId: '678', price: 80 },
+    ]},
+    { orderId: '456', customerId: '345', deliveryDate: '12-01-2020', delivered: true, items: [
+        { productId: '789', price: 12 },
+        { productId: '890', price: 90 },
+    ]},
+        { orderId: '578', customerId: '456', deliveryDate: '12-01-2020', delivered: true, items: [
+        { productId: '901', price: 43 },
+        { productId: '123', price: 55 },
+    ]},
+];
+
+// Exercises
+
+// 1) Get a list of the orders for the customer with the ID 234 that have not been delivered.
+console.log(orders.filter(order => order.customerId === '234' && !order.delivered))
+
+
+// 2) Create a new property on each order with the total price of items ordered.
+
+
+// 3) Have all the orders been delivered?
+
+
+// 4) Has the customer with ID '123' made any orders?
+
+
+// 5) Have any products with an id of 123 been sold?
