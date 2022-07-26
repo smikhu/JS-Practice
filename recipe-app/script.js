498f46bcbc39c33c689108fd9021911197276c45
Steven Mikhu <StevenMikhu@gmail.com>
2022-07-25 23:03:08 -0400

    random meal properly showing up

--- recipe-app/script.js
+++ recipe-app/script.js
@@ -1,3 +1,5 @@
+const meals = document.getElementById("meals");
+
 getRandomMeal();
 
 async function getRandomMeal() {
@@ -9,7 +11,9 @@ async function getRandomMeal() {
 
   const randomMeal = responseData.meals[0];
 
-  console.log(responseData.meals[0]);
+  console.log(randomMeal);
+
+  addMeal(randomMeal, true);
 }
 
 async function getMealById(id) {
@@ -22,4 +26,23 @@ async function getMealsBySearch(term) {
   const meals = await fetch(
     "https://www.themealdb.com/api/json/v1/1/search.php?s=" + term
   );
+}
+
+function addMeal(mealData, random = false) {
+    const meal = document.createElement('div');
+    meal.classList.add('meal');
+
+    meal.innerHTML = `
+                <div class="meal-header">
+                    ${random ? `<span class="random">Random Recipe</span>` : ''}
+                    <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
+                </div>
+                <div class="meal-body">
+                    <h4>${mealData.strMeal}</h4>
+                    <button class="fav-btn"><i class="fas fa-heart"></i></button>
+                </div>
+            
+    `;
+
+    meals.appendChild(meal)
 }
