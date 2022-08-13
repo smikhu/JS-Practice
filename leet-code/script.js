b3637d2095572be3680b3dea39fa7d001d447a02
Steven Mikhu <StevenMikhu@gmail.com>
2022-08-12 22:41:09 -0400

    longest common prefix

--- leet-code/script.js
+++ leet-code/script.js
@@ -166,9 +166,29 @@ const romanToInt2 = (s) => {
   return ans.reduce((total, curr) => total + curr, 0)
 };
 
-console.log(romanToInt2("I"));
-console.log(romanToInt2("III"));
-console.log(romanToInt2("IV"));
-console.log(romanToInt2("IX"));
-console.log(romanToInt2("LVIII"));
-console.log(romanToInt2("MCMXCIV"));
+// console.log(romanToInt2("I"));
+// console.log(romanToInt2("III"));
+// console.log(romanToInt2("IV"));
+// console.log(romanToInt2("IX"));
+// console.log(romanToInt2("LVIII"));
+// console.log(romanToInt2("MCMXCIV"));
+
+
+
+const longestCommonPrefix = (arr) => {
+  let prefix = '';
+  if (arr.length === 0) return prefix
+
+  for (let i = 0; i < arr[0].length; i++) {
+    console.log(arr[0][i])
+    const character = arr[0][i]
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j][i] !== character) return prefix
+    }
+    prefix = prefix + character
+  }
+  return prefix
+}
+
+console.log(longestCommonPrefix(['flower', 'flow', 'flight']))
+console.log(longestCommonPrefix(['dog', 'racecar', 'car']))
