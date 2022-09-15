294127ba9cc8aacb3e27dfb0e3e1d27e74ddd405
Steven Mikhu <StevenMikhu@gmail.com>
2022-09-14 22:31:11 -0400

    days looped on and amount of days in each month properly showing dynamicly

--- calender/script.js
+++ calender/script.js
@@ -1,7 +1,10 @@
 const monthsElement = document.querySelector('.date h1')
 const fullDateElement = document.querySelector('.date p')
+const daysElement = document.querySelector('.days')
 
 const monthIndex = new Date().getMonth();
+const lastDay = new Date(new Date().getFullYear(), monthIndex + 1, 0).getDate();
+console.log(lastDay)
 
 const months = [
   "January",
@@ -20,3 +23,12 @@ const months = [
 
 monthsElement.innerText = months[monthIndex]
 fullDateElement.innerText = new Date().toDateString()
+
+
+let days = "";
+
+for(let i = 1; i <= lastDay; i++) {
+    days = days + `<div>${i}</div>`
+}
+
+daysElement.innerHTML = days
