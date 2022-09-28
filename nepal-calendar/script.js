a6117387f86af9d61c7d38916564fe1c86e0da41
Steven Mikhu <StevenMikhu@gmail.com>
2022-09-27 22:45:25 -0400

    current date showing as active

--- nepal-calendar/script.js
+++ nepal-calendar/script.js
@@ -29,6 +29,8 @@ const renderCalendar = () => {
 
   let lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate();
 
+  let lastDayOfMonth = new Date(currYear, currMonth, lastDateOfMonth).getDay();
+
   let lastDateOfLastMonth = new Date(currYear, currMonth, 0).getDate();
 
   let liTag = "";
@@ -38,7 +40,17 @@ const renderCalendar = () => {
   }
 
   for (let i = 1; i <= lastDateOfMonth; i++) {
-    liTag += `<li>${i}</li>`;
+    let isToday =
+      i === date.getDate() &&
+      currMonth === new Date().getMonth() &&
+      currYear === new Date().getFullYear()
+        ? "active"
+        : "";
+    liTag += `<li class="${isToday}">${i}</li>`;
+  }
+
+  for (let i = lastDayOfMonth; i < 6; i++) {
+    liTag += `<li class="inactive">${i - lastDayOfMonth + 1}</li>`;
   }
 
   currentDate.innerText = `${months[currMonth]} ${currYear}`;
