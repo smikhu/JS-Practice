f3523074c700532fe72c36a543b7ab9a0a0c1a4e
Steven Mikhu <StevenMikhu@gmail.com>
2022-10-08 03:32:10 -0400

    insert method added to doubly linked list

--- data-structures-and-algorithms/script.js
+++ data-structures-and-algorithms/script.js
@@ -231,6 +231,20 @@ class DoublyLinkedList {
     }
     return false;
   }
+
+  insert(index, value) {
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
+    if (index < 0 || index > this.length) return false;
+
+    const newNode = new Node2(value);
+    const before = this.get(index - 1);
+    const after = before.next;
+    before.next = newNode;
+    newNode.prev = before;
+    newNode.next = after;
+    after.prev = newNode;
+  }
 }
 
 let myDoublyLinkedList = new DoublyLinkedList(7);
