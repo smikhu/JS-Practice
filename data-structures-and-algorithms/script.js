a1180f78c79f89cb53cc261b2ff2412590071a2f
Steven Mikhu <StevenMikhu@gmail.com>
2022-10-08 01:54:22 -0400

    remove method added

--- data-structures-and-algorithms/script.js
+++ data-structures-and-algorithms/script.js
@@ -102,6 +102,20 @@ class LinkedList {
     this.length++;
     return true;
   }
+
+  remove() {
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    if (index < 0 || index >= this.length) return undefined;
+
+    const before = this.get(index - 1);
+    const temp = before.next;
+
+    before.next = temp.next;
+    temp.next = null;
+    this.length--;
+    return temp;
+  }
 }
 
 let myLinkedList = new LinkedList(7);
