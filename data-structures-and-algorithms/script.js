8d57153e271340b6642ff0fe87dea5cb0ee90b6c
Steven Mikhu <StevenMikhu@gmail.com>
2022-10-08 01:31:46 -0400

    get method added

--- data-structures-and-algorithms/script.js
+++ data-structures-and-algorithms/script.js
@@ -68,6 +68,17 @@ class LinkedList {
     }
     return temp;
   }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      return undefined;
+    }
+    let temp = this.head;
+    for (let i = 0; i < index; i++) {
+      temp = temp.next;
+    }
+    return temp;
+  }
 }
 
 let myLinkedList = new LinkedList(7);
