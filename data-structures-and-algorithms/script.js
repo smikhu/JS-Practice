a91eb4948d4918bb34b1eda50df1d2c0187b836f
Steven Mikhu <StevenMikhu@gmail.com>
2022-10-08 01:23:22 -0400

    shift method added

--- data-structures-and-algorithms/script.js
+++ data-structures-and-algorithms/script.js
@@ -56,6 +56,18 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  shift() {
+    if (!this.head) return undefined;
+    let temp = this.head;
+    this.head = this.head.next;
+    temp.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return temp;
+  }
 }
 
 let myLinkedList = new LinkedList(7);
