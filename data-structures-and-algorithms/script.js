fec51a8b7272e23fdd77a47e0c4a0c484fa833f8
Steven Mikhu <StevenMikhu@gmail.com>
2022-10-09 17:08:14 -0400

    insert method added to binary search tree

--- data-structures-and-algorithms/script.js
+++ data-structures-and-algorithms/script.js
@@ -366,6 +366,31 @@ class BST {
   constructor() {
     this.root = null;
   }
+
+  insert(value) {
+    const newNode = new Node5(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let temp = this.root;
+    while (true) {
+      if (newNode.value === temp.value) return undefined;
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+        temp = temp.left;
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+        temp = temp.right;
+      }
+    }
+  }
 }
 
 let myTree = new BST();
