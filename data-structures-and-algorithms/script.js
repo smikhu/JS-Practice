1fd6284b8e8c247c9b582cbe2caba4506f080120
Steven Mikhu <StevenMikhu@gmail.com>
2022-10-08 23:45:23 -0400

    dequeue method added

--- data-structures-and-algorithms/script.js
+++ data-structures-and-algorithms/script.js
@@ -306,36 +306,36 @@
 // let myStack = new Stack(0);
 // myStack;
 
-class Node4 {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
+// class Node4 {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
 
-class Queue {
-  constructor(value) {
-    const newNode = new Node4(value);
-    this.first = newNode;
-    this.last = newNode;
-    this.length = 1;
-  }
+// class Queue {
+//   constructor(value) {
+//     const newNode = new Node4(value);
+//     this.first = newNode;
+//     this.last = newNode;
+//     this.length = 1;
+//   }
 
-  enqueue(value) {
-    const newNode = new Node4(value);
-    if (this.length === 0) {
-      this.first = newNode;
-      this.last = newNode;
-    } else {
-      this.last.next = newNode;
-      this.last = newNode;
-    }
-    this.length++;
-    return this;
-  }
-}
+//   enqueue(value) {
+//     const newNode = new Node4(value);
+//     if (this.length === 0) {
+//       this.first = newNode;
+//       this.last = newNode;
+//     } else {
+//       this.last.next = newNode;
+//       this.last = newNode;
+//     }
+//     this.length++;
+//     return this;
+//   }
+// }
 
-let myQueue = new Queue(4);
-myQueue.enqueue(5);
-myQueue.enqueue(6);
-myQueue;
+// let myQueue = new Queue(4);
+// myQueue.enqueue(5);
+// myQueue.enqueue(6);
+// myQueue;
