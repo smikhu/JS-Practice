9d4328a259023a4d9bbd4aa302543f0b79f6508d
Steven Mikhu <StevenMikhu@gmail.com>
2022-10-10 21:01:03 -0400

    hash table created

--- data-structures-and-algorithms/script.js
+++ data-structures-and-algorithms/script.js
@@ -354,73 +354,89 @@
 // myQueue.enqueue(6);
 // myQueue;
 
-class Node5 {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
+// class Node5 {
+//   constructor(value) {
+//     this.value = value;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+// class BST {
+//   constructor() {
+//     this.root = null;
+//   }
+
+//   insert(value) {
+//     const newNode = new Node5(value);
+//     if (this.root === null) {
+//       this.root = newNode;
+//       return this;
+//     }
+//     let temp = this.root;
+//     while (true) {
+//       if (newNode.value === temp.value) return undefined;
+//       if (newNode.value < temp.value) {
+//         if (temp.left === null) {
+//           temp.left = newNode;
+//           return this;
+//         }
+//         temp = temp.left;
+//       } else {
+//         if (temp.right === null) {
+//           temp.right = newNode;
+//           return this;
+//         }
+//         temp = temp.right;
+//       }
+//     }
+//   }
+
+//   contains(value) {
+//     if (this.root === null) return false;
+//     let temp = this.root;
+//     while (temp) {
+//       if (value < temp.value) {
+//         temp = temp.left;
+//       } else if (value > temp.value) {
+//         temp = temp.right;
+//       } else {
+//         return true;
+//       }
+//     }
+//     return false;
+//   }
+
+//   minValueNode(currentNode) {
+//     while (currentNode.left != null) {
+//       currentNode = currentNode.left;
+//     }
+//     return currentNode;
+//   }
+// }
+
+// let myTree = new BST();
+// myTree.insert(47);
+// myTree.insert(21);
+// myTree.insert(76);
+// myTree.insert(18);
+// myTree.insert(27);
+// myTree.insert(52);
+// myTree.insert(82);
+// myTree.minValueNode(myTree.root.right);
+
+class HashTable {
+  constructor(size = 7) {
+    this.dataMap = new Array(size);
+  }
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
+    }
+    return hash;
   }
 }
 
-class BST {
-  constructor() {
-    this.root = null;
-  }
-
-  insert(value) {
-    const newNode = new Node5(value);
-    if (this.root === null) {
-      this.root = newNode;
-      return this;
-    }
-    let temp = this.root;
-    while (true) {
-      if (newNode.value === temp.value) return undefined;
-      if (newNode.value < temp.value) {
-        if (temp.left === null) {
-          temp.left = newNode;
-          return this;
-        }
-        temp = temp.left;
-      } else {
-        if (temp.right === null) {
-          temp.right = newNode;
-          return this;
-        }
-        temp = temp.right;
-      }
-    }
-  }
-
-  contains(value) {
-    if (this.root === null) return false;
-    let temp = this.root;
-    while (temp) {
-      if (value < temp.value) {
-        temp = temp.left;
-      } else if (value > temp.value) {
-        temp = temp.right;
-      } else {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  minValueNode(currentNode) {
-    while (currentNode.left != null) {
-      currentNode = currentNode.left;
-    }
-    return currentNode;
-  }
-}
-
-let myTree = new BST();
-myTree.insert(47);
-myTree.insert(21);
-myTree.insert(76);
-myTree.insert(18);
-myTree.insert(27);
-myTree.insert(52);
-myTree.insert(82);
-myTree.minValueNode(myTree.root.right);
+let myHashTable = new HashTable();
+myHashTable;
