f99246655eac352760cac48f7f17629ecdd6097b
Steven Mikhu <StevenMikhu@gmail.com>
2022-10-10 21:14:41 -0400

    set method added to hash table

--- data-structures-and-algorithms/script.js
+++ data-structures-and-algorithms/script.js
@@ -425,18 +425,28 @@
 // myTree.insert(82);
 // myTree.minValueNode(myTree.root.right);
 
-class HashTable {
-  constructor(size = 7) {
-    this.dataMap = new Array(size);
-  }
-  _hash(key) {
-    let hash = 0;
-    for (let i = 0; i < key.length; i++) {
-      hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
-    }
-    return hash;
-  }
-}
+// class HashTable {
+//   constructor(size = 7) {
+//     this.dataMap = new Array(size);
+//   }
 
-let myHashTable = new HashTable();
-myHashTable;
+//   _hash(key) {
+//     let hash = 0;
+//     for (let i = 0; i < key.length; i++) {
+//       hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
+//     }
+//     return hash;
+//   }
+
+//   set(key, value) {
+//     let index = this._hash(key);
+//     if (!this.dataMap[index]) {
+//       this.dataMap[index] = [];
+//     }
+//     this.dataMap[index].push([key, value]);
+//     return this;
+//   }
+// }
+
+// let myHashTable = new HashTable();
+// myHashTable;
