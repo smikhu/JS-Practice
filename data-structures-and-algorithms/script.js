cf6def5fa629828fd2879ab7d0c0b9d3a8d39fe9
Steven Mikhu <StevenMikhu@gmail.com>
2022-10-14 21:14:29 -0400

    adding edge to graph

--- data-structures-and-algorithms/script.js
+++ data-structures-and-algorithms/script.js
@@ -487,6 +487,15 @@ class Graph {
     }
     return false;
   }
+
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1);
+      return true;
+    }
+    return false;
+  }
 }
 
 let myGraph = new Graph();
