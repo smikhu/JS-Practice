d904e3d88eca0175318b1ff3089e9382ab5105ac
Steven Mikhu <StevenMikhu@gmail.com>
2022-10-24 23:09:06 -0400

    merge sort added

--- data-structures-and-algorithms/script.js
+++ data-structures-and-algorithms/script.js
@@ -605,3 +605,16 @@ function merge(array1, array2) {
 }
 
 merge([1, 3, 7, 8], [2, 4, 5, 6]);
+
+
+function mergeSort(array) {
+    if (array.length === 1) return array
+
+    let mid = Math.floor(array.length/2)
+    let left = array.slice(0, mid)
+    let right = array.slice(mid)
+
+    return merge(mergeSort(left), mergeSort(right))
+}
+
+mergeSort([3, 1, 4, 2])
