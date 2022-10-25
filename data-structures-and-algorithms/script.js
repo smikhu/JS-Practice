b3fa4df1367d8b2fa23a6b96443d7f2a4c28812c
Steven Mikhu <StevenMikhu@gmail.com>
2022-10-24 22:17:21 -0400

    selection sort added

--- data-structures-and-algorithms/script.js
+++ data-structures-and-algorithms/script.js
@@ -533,17 +533,35 @@
 // myGraph.addEdge("C", "D");
 // myGraph;
 
-function bubbleSort(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    for (let j = 0; j < i; j++) {
-      if (array[j] > array[j + 1]) {
-        let temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
-      }
+// function bubbleSort(array) {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     for (let j = 0; j < i; j++) {
+//       if (array[j] > array[j + 1]) {
+//         let temp = array[j];
+//         array[j] = array[j + 1];
+//         array[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return array;
+// }
+
+// bubbleSort([4, 2, 6, 5, 1, 3]);
+
+function selectionSort(array) {
+  let min;
+  for (let i = 0; i < array.length - 1; i++) {
+    min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) min = j;
+    }
+    if (i !== min) {
+      let temp = array[i];
+      array[i] = array[min];
+      array[min] = temp;
     }
   }
   return array;
 }
 
-bubbleSort([4, 2, 6, 5, 1, 3]);
+selectionSort([4, 2, 6, 5, 1, 3, 8, 7, 10, 9]);
