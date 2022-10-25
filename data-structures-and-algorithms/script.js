31d359c353eaf5f552f35c2e1a5524220f064efc
Steven Mikhu <StevenMikhu@gmail.com>
2022-10-24 22:43:19 -0400

    insertion sort added

--- data-structures-and-algorithms/script.js
+++ data-structures-and-algorithms/script.js
@@ -548,20 +548,34 @@
 
 // bubbleSort([4, 2, 6, 5, 1, 3]);
 
-function selectionSort(array) {
-  let min;
-  for (let i = 0; i < array.length - 1; i++) {
-    min = i;
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[j] < array[min]) min = j;
+// function selectionSort(array) {
+//   let min;
+//   for (let i = 0; i < array.length - 1; i++) {
+//     min = i;
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (array[j] < array[min]) min = j;
+//     }
+//     if (i !== min) {
+//       let temp = array[i];
+//       array[i] = array[min];
+//       array[min] = temp;
+//     }
+//   }
+//   return array;
+// }
+
+// selectionSort([4, 2, 6, 5, 1, 3, 8, 7, 10, 9]);
+
+function insertionSort(array) {
+  let temp;
+  for (let i = 1; i < array.length; i++) {
+    temp = array[i];
+    for (var j = i - 1; array[j] > temp && j > -1; j--) {
+      array[j + 1] = array[j];
     }
-    if (i !== min) {
-      let temp = array[i];
-      array[i] = array[min];
-      array[min] = temp;
-    }
+    array[j + 1] = temp;
   }
   return array;
 }
 
-selectionSort([4, 2, 6, 5, 1, 3, 8, 7, 10, 9]);
+insertionSort([4, 2, 6, 5, 1, 3]);
