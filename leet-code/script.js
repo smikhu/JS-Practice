79830f032cd7aa4a8c14ee3587f2ed4bb9a47ac1
Steven Mikhu <StevenMikhu@gmail.com>
2022-10-26 21:41:33 -0400

    roman to integer solved

--- leet-code/script.js
+++ leet-code/script.js
@@ -173,6 +173,36 @@ const romanToInt2 = (s) => {
 // console.log(romanToInt2("LVIII"));
 // console.log(romanToInt2("MCMXCIV"));
 
+const intToRoman = (num) => {
+  let romanToNum = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  let roman = "";
+
+  for (let key in romanToNum) {
+    while (num >= romanToNum[key]) {
+      roman = roman + key;
+      num = num - romanToNum[key];
+    }
+  }
+  return roman;
+};
+
+console.log(intToRoman(30));
+
 const longestCommonPrefix = (arr) => {
   let prefix = "";
   if (arr.length === 0) return prefix;
@@ -256,7 +286,7 @@ const findDuplicates = (nums) => {
 // console.log(findDuplicates([4, 3, 2, 2, 3, 1]));
 
 const findDuplicates2 = (nums) => {
-  return nums.filter((item, index) => nums.indexOf(item) !== index );
+  return nums.filter((item, index) => nums.indexOf(item) !== index);
 };
 
-console.log(findDuplicates2([4, 3, 2, 2, 3, 1]));
+// console.log(findDuplicates2([4, 3, 2, 2, 3, 1]));
